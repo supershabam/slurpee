720586ceb666eaaacb7c0a47ae0818d7af1caf89
Ian Hansen <ian@supershabam.com>
2014-02-04 19:09:05 -0700

    redis client from uri

--- slurpee.go
+++ slurpee.go
@@ -1,8 +1,12 @@
 package slurpee
 
 import (
+	"errors"
+	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"io"
+	"net/url"
+	"strings"
 )
 
 type slurpee struct {
@@ -14,7 +18,7 @@ type slurpee struct {
 func NewSlurpee(redisUrl string, channel string) *slurpee {
 	s := slurpee{}
 	s.C = make(chan []byte)
-	conn, err := redis.Dial("tcp", ":6379")
+	conn, err := newRedisConn(redisUrl)
 	if err != nil {
 		s.Err = err
 		close(s.C)
@@ -44,4 +48,43 @@ func NewSlurpee(redisUrl string, channel string) *slurpee {
 func (s *slurpee) Stop() {
 	s.conn.Close()
 	return
+}
+
+func newRedisConn(uri string) (redis.Conn, error) {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+	var network string
+	var host string
+	var auth string
+
+	switch u.Scheme {
+	case "redis":
+		network = "tcp"
+		host = u.Host
+		if !strings.Contains(host, ":") {
+			host = fmt.Sprintf("%s:6379", host)
+		}
+		if u.User != nil {
+			auth, _ = u.User.Password()
+		}
+	default:
+		return nil, errors.New("invalid redis uri scheme")
+	}
+
+	conn, err := redis.Dial(network, host)
+	if err != nil {
+		return nil, err
+	}
+
+	if auth != "" {
+		_, err = conn.Do("AUTH", auth)
+		if err != nil {
+			conn.Close()
+			return nil, err
+		}
+	}
+
+	return conn, nil
 }
